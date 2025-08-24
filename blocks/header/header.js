8c3f5b95c18ccbd6272b9721d85def5e470c3f5a
learningeds <thrinayaedslearning@gmail.com>
2025-08-24 14:29:49 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -170,7 +170,9 @@ export default async function decorate(block) {
 
   // Assemble navigation
   nav.append(navBrand, hamburger, navSections);
-  navWrapper.append(nav);
+// Correct DOM structure for layout
+navWrapper.append(nav, navSections); // NOT nav.append(navSections)
+
   block.append(navWrapper);
 }
 

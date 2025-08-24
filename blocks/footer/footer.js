475d18a8ae98cb8195c96f66e7c3d408d22a15dc
learningeds <thrinayaedslearning@gmail.com>
2025-08-24 19:48:01 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,40 +1,20 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
-import { getThemeFromUrl } from '../../scripts/scripts.js';
 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  // Load footer as fragment
-//  const footerMeta = getMetadata('footer');
-//  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  // load footer as fragment
   const footerMeta = getMetadata('footer');
-  let footerPath;
-
-  if (footerMeta) {
-    footerPath = new URL(footerMeta, window.location).pathname;
-  } else {
-    const theme = getThemeFromUrl();
-        switch (theme) {
-          case 'midsouth':
-            footerPath = '/midsouth-aem-boilerplate/footer';
-            break;
-          default:
-            footerPath = '/footer';
-            break;
-        }
-  }
-
+  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
 
-  // Clear the current footer block content
+  // decorate footer DOM
   block.textContent = '';
+  const footer = document.createElement('div');
+  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
-  // Instead of appending a new wrapper, flatten the fragment's children into the block
-  while (fragment.firstElementChild) {
-    const section = fragment.firstElementChild;
-    block.append(section);
-  }
+  block.append(footer);
 }
